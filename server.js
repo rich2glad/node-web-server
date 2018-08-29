068155751cbe83e66a622a7de6d1ae0d134fde52
rich2glad <rich2glad@gmail.com>
2018-08-29 13:47:42 -0400

    Initial  Commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}:${req.url}`;
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err){
+            console.log(err)
+        }
+    });
+    console.log(log);
+    next();
+});
+
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs',{
+        pageTitle :'Maintenance Page'
+    });
+    
+});
+app.use(express.static( __dirname +'/public'));
+hbs.registerHelper('getCurrentYear',()=> {
+return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=> {
+    return text.toUpperCase();
+    });
+
+
+app.get('/',(req,res)=>{
+    // res.send('<h1>Hello Express</h1>');
+    // res.send({
+    //     name: 'Richerd',
+    //     likes :[
+    //         'Biking','Swimming'
+    //     ]
+    // });
+    res.render('home.hbs',{
+        pageTitle :'Home Page',
+        welcomePage: 'Welcome to some Website'
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle :'About Page'
+    });
+});
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        error: 'Unable to process',
+    });
+});
+
+app.listen(3000,()=>{
+    console.log('server is up in port 3000');
+});
